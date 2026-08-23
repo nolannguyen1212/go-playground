f66374855b7ec12e4d4ce5b20e58d91186e384bd
william1nguyen <vietanhhd03@gmail.com>
2026-08-23 11:11:04 +0700

    test(pubsub): cover publish routing by topic

---
+++ internal/pubsub_test.go
@@ -0,0 +1,43 @@
+package internal_test
+
+import (
+	"testing"
+
+	"github.com/go-jose/go-jose/v4/testutils/assert"
+	"github.com/nolannguyen1212/go-runbook/internal"
+)
+
+func newBroker(t *testing.T) *internal.Broker {
+	t.Helper()
+
+	broker := internal.NewBroker()
+	t.Cleanup(broker.Close)
+
+	return broker
+}
+
+func TestPublishByTopic(t *testing.T) {
+	broker := newBroker(t)
+
+	a := broker.Subscribe(internal.TopicA)
+	b := broker.Subscribe(internal.TopicB)
+
+	broker.Publish(internal.TopicA, "hello A")
+	assert.Equal(t, <-a, "hello A")
+
+	select {
+	case msg := <-b:
+		t.Fatalf("b should receive nothing, got %v", msg)
+	default:
+	}
+}
+
+func TestUnsubscribe(t *testing.T) {
+	broker := newBroker(t)
+
+	sub := broker.Subscribe(internal.TopicA)
+	broker.Unsubscribe(sub)
+
+	_, ok := <-sub
+	assert.Equal(t, ok, false)
+}
